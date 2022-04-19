372864a98e05c0a7fd2a9420b703f83647bfece6
rikurikuman <k021g1447@g.neec.ac.jp>
2022-04-19 15:11:21 +0900

    02_資料終わり

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,11 +6,13 @@ public class PlayerScript : MonoBehaviour
 {
     public Camera cameraObj;
     public float power = 10;
+    public AudioSource audioSource;
+    public AudioClip goalSE;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -33,10 +35,20 @@ public class PlayerScript : MonoBehaviour
             gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -1) * power);
         }
 
-
         Vector3 vec = new Vector3(0, 20, -20);
         Vector3 pos = this.gameObject.transform.position;
         pos += vec;
         cameraObj.transform.SetPositionAndRotation(pos, Quaternion.Euler(45, 0, 0));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Goal")
+        {
+            audioSource.Pause();
+            audioSource.PlayOneShot(goalSE);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1000, 0));
+            Debug.Log("ÉSÅ[ÉãÅI");
+        }
     }
 }
