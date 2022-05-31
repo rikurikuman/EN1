038c0b08ad94d7499ea65a925a5b7f397893a744
rikurikuman <k021g1447@g.neec.ac.jp>
2022-05-31 13:16:56 +0900

    03_資料終わり

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    float rot = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rot += Time.deltaTime * 360;
+        transform.rotation = Quaternion.Euler(90, rot, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
